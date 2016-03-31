d0ce9068870aaba649de392d77c0014281ba0e36
HeBing <bhe3@jhu.edu>
2016-03-31 11:34:36 -0400

    added data examples and updated demo.sh

--- src/demo.sh
+++ src/demo.sh
@@ -1,9 +1,9 @@
 # count read coverage for bins
-./countCoverage -i bamFilesList -p param
+./countCoverage -i ./data/bamFilesList -p ./data/param
 # extract bin-wise log2 coverage for training genomic region
-./extractFeature -i coverageFilesList -t trainingCMYC.txt -o feature_trainingCMYC.txt -p param
+./extractFeature -i ./data/coverageFilesList -t ./data/trainingCMYC.txt -o feature_trainingCMYC.txt -p param
 # extract bin-wise log2 coverage for testing genomic region
-./extractFeature -i coverageFilesList -t testingGABP.txt -o feature_testingGABP.txt -p param
+./extractFeature -i ./data/coverageFilesList -t ./data/testingGABP.txt -o feature_testingGABP.txt -p ./data/param
 # make prediction with fcat
-python fcat.py -model RandomForest,LogisticRegressionL1 -train ../data/feature_trainingCMYC.txt_5_1000 -test ../data/feature_trainingGABP.txt_5_1000 -output finalResult.txt
+python fcat.py -model RandomForest,LogisticRegressionL1 -train ./data/feature_trainingCMYC.txt_5_1000 -test ./data/feature_trainingGABP.txt_5_1000 -output ./data/finalResult.txt
 
