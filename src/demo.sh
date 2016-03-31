046371c7476f04fcf124faeaaee9b1fd34441f03
HeBing <bhe3@jhu.edu>
2016-03-31 11:22:58 -0400

    added demo cmd

--- src/demo.sh
+++ src/demo.sh
@@ -1,2 +1,9 @@
-python modelAveragingWapper.py -model RandomForest,LogisticRegressionL1 -train ../examples/CMYCOutputFeature_2cat_ATACseq.txt_5_1000 -test ../examples/CTCFOutputFeature_2cat_ATACseq.txt_5_1000 -output default
+# count read coverage for bins
+./countCoverage -i bamFilesList -p param
+# extract bin-wise log2 coverage for training genomic region
+./extractFeature -i coverageFilesList -t trainingCMYC.txt -o feature_trainingCMYC.txt -p param
+# extract bin-wise log2 coverage for testing genomic region
+./extractFeature -i coverageFilesList -t testingGABP.txt -o feature_testingGABP.txt -p param
+# make prediction with fcat
+python fcat.py -model RandomForest,LogisticRegressionL1 -train ../data/feature_trainingCMYC.txt_5_1000 -test ../data/feature_trainingGABP.txt_5_1000 -output finalResult.txt
 
